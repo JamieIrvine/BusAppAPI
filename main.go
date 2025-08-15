663d565f346bd8de2fa4dfd2059851d7c6f74035
Jamie Irvine <jamie.irvine14@gmail.com>
2025-08-15 22:02:30 +0100

    Filter on bus service

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +13,7 @@ import (
 type TranslinkLiveBus struct {
 	ID                string `json:"ID"`
 	Operator          string `json:"Operator"`
-	JourneyIdentifier string `json:"JourneyIdentifier"`
+	JourneyIdentifier string `json:"JourneyID"`
 	DayOfOperation    string `json:"DayOfOperation"`
 	Delay             int    `json:"Delay"`
 	MOTCode           int    `json:"MOTCode"`
@@ -25,6 +27,62 @@ type TranslinkLiveBus struct {
 	RealtimeAvailable int    `json:"RealtimeAvailable"`
 	LineText          string `json:"LineText"`
 	DirectionText     string `json:"DirectionText"`
+}
+
+type LiveBusOutput struct {
+	ID                  string `json:"id"`
+	Operator            string `json:"operator"`
+	JourneyID           string `json:"journeyID"`
+	DateOfOperation     string `json:"dateOfOperation"`
+	DelaySeconds        int    `json:"delaySeconds"`
+	LastUpdated         string `json:"lastUpdated"`
+	LastUpdatedPrevious string `json:"lastUpdatedPrevious"`
+	XCurrent            string `json:"xCurrent"`
+	YCurrent            string `json:"yCurrent"`
+	XPrevious           string `json:"xPrevious"`
+	YPrevious           string `json:"yPrevious"`
+	VehicleID           string `json:"vehicleID"`
+	RealtimeAvailable   int    `json:"realtimeAvailable"`
+	ServiceNumber       string `json:"serviceNumber"`
+	Direction           string `json:"direction"`
+}
+
+func convertLiveBuses(buses []TranslinkLiveBus) []LiveBusOutput {
+
+	result := make([]LiveBusOutput, 0)
+
+	for _, b := range buses {
+
+		// Parse using the correct layout
+		var dateStr string
+		t, err := time.Parse("02.01.2006", b.DayOfOperation)
+		if err != nil {
+			log.Printf("Error parsing day of operation date: %v", err)
+			dateStr = ""
+		} else {
+			dateStr = t.Format("2006-01-02")
+		}
+
+		result = append(result, LiveBusOutput{
+			ID:                  b.ID,
+			Operator:            b.Operator,
+			JourneyID:           b.JourneyIdentifier,
+			DateOfOperation:     dateStr,
+			DelaySeconds:        b.Delay,
+			XCurrent:            b.X,
+			YCurrent:            b.Y,
+			LastUpdated:         b.Timestamp,
+			XPrevious:           b.XPrevious,
+			YPrevious:           b.YPrevious,
+			LastUpdatedPrevious: b.TimestampPrevious,
+			VehicleID:           b.VehicleIdentifier,
+			RealtimeAvailable:   b.RealtimeAvailable,
+			ServiceNumber:       b.LineText,
+			Direction:           b.DirectionText,
+		})
+	}
+
+	return result
 }
 
 func main() {
@@ -45,7 +103,22 @@ func main() {
 			log.Fatal(err)
 		}
 
-		c.JSON(http.StatusOK, buses)
+		converted := convertLiveBuses(buses)
+
+		serviceNumberParam := c.Query("serviceNumber")
+		if serviceNumberParam != "" {
+
+			allBuses := converted
+			converted = make([]LiveBusOutput, 0)
+
+			for _, b := range allBuses {
+				if strings.ToLower(b.ServiceNumber) == strings.ToLower(serviceNumberParam) {
+					converted = append(converted, b)
+				}
+			}
+		}
+
+		c.JSON(http.StatusOK, converted)
 	})
 
 	if err := r.Run(":8080"); err != nil {
